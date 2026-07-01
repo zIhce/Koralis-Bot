080eab7ba2ae82e36b46dfc173141f5573d0a8a6
zIhce <zihce01@gmail.com>
2026-07-01 11:56:16 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Bot Creado Para Koralis Network",
         // Activity type number (0 = Playing).
         type: 0,
       },
